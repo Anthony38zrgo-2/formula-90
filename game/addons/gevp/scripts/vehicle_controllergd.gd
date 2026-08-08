172cfc8536f02f2568d7e8f530a83e73ffbd7796
Anthony38zrgo-2 <zrgo38@hotmail.com>
2026-08-08 13:06:37 -0500

    refactor: restore upstream GEVP controller

--- game/addons/gevp/scripts/vehicle_controllergd.gd
+++ game/addons/gevp/scripts/vehicle_controllergd.gd
@@ -1,28 +1,56 @@
 extends Node3D
+## Controls any [Vehicle] node using custom-defined input maps.
 class_name VehicleController
 
+## The [Vehicle] that this vehicle controller will send
+## input values to. Required for the vehicle controller to work properly.
 @export var vehicle_node : Vehicle
 
 @export_group("Input Maps", "string_")
+## The name of the input map used for this vehicle's brakes input.
+## [br]The input map must be present in your project, and can be set at [code]Project > Project Settings > Input Map[/code].
+## [br]Leave blank to disable.
 @export var string_brake_input: String = "Brakes"
+## The name of the input map used for steering this vehicle left.
+## [br]The input map must be present in your project, and can be set at [code]Project > Project Settings > Input Map[/code].
+## [br]Leave blank to disable.
 @export var string_steer_left: String = "Steer Left"
+## The name of the input map used for steering this vehicle right.
+## [br]The input map must be present in your project, and can be set at [code]Project > Project Settings > Input Map[/code].
+## [br]Leave blank to disable.
 @export var string_steer_right: String = "Steer Right"
+## The name of the input map used for this vehicle's throttle input.
+## [br]The input map must be present in your project, and can be set at [code]Project > Project Settings > Input Map[/code].
+## [br]Leave blank to disable.
 @export var string_throttle_input: String = "Throttle"
+## The name of the input map used for this vehicle's handbrake input.
+## [br]The input map must be present in your project, and can be set at [code]Project > Project Settings > Input Map[/code].
+## [br]Leave blank to disable.
 @export var string_handbrake_input: String = "Handbrake"
+## The name of the input map used for this vehicle's clutch input.
+## [br]The input map must be present in your project, and can be set at [code]Project > Project Settings > Input Map[/code].
+## [br]Leave blank to disable.
 @export var string_clutch_input: String = "Clutch"
+## The name of the input map used for enabling or disabling
+## the transmission of this vehicle.
+## [br]The input map must be present in your project, and can be set at [code]Project > Project Settings > Input Map[/code].
+## [br]Leave blank to disable.
 @export var string_toggle_transmission: String = "Toggle Transmission"
+## The name of the input map used for shifting up a gear when
+## manual transmission is enabled.
+## [br]The input map must be present in your project, and can be set at [code]Project > Project Settings > Input Map[/code].
+## [br]Leave blank to disable.
 @export var string_shift_up: String = "Shift Up"
+## The name of the input map used for shifting down a gear when
+## manual transmission is enabled.
+## [br]The input map must be present in your project, and can be set at [code]Project > Project Settings > Input Map[/code].
+## [br]Leave blank to disable.
 @export var string_shift_down: String = "Shift Down"
 
-const STOP_SPEED = 0.1
-const INPUT_THRESHOLD = 0.5
-
-var _brake_was_down = false
-var _throttle_was_down = false
-
 func _physics_process(_delta):
-	if not vehicle_node:
-		return
+	
+	if string_brake_input != "":
+		vehicle_node.brake_input = Input.get_action_strength(string_brake_input)
 
 	if string_steer_left != "" and string_steer_right != "":
 		vehicle_node.steering_input = Input.get_action_strength(string_steer_left) - Input.get_action_strength(string_steer_right)
@@ -30,41 +58,26 @@ func _physics_process(_delta):
 	if string_throttle_input != "":
 		vehicle_node.throttle_input = pow(Input.get_action_strength(string_throttle_input), 2.0)
 
-	if string_brake_input != "":
-		vehicle_node.brake_input = Input.get_action_strength(string_brake_input)
-
 	if string_handbrake_input != "":
 		vehicle_node.handbrake_input = Input.get_action_strength(string_handbrake_input)
-
+	
 	if string_clutch_input != "":
 		vehicle_node.clutch_input = clampf(Input.get_action_strength(string_clutch_input) + Input.get_action_strength(string_handbrake_input), 0.0, 1.0)
-
+	
 	if string_toggle_transmission != "":
 		if Input.is_action_just_pressed(string_toggle_transmission):
 			vehicle_node.automatic_transmission = not vehicle_node.automatic_transmission
-
+	
 	if string_shift_up != "":
 		if Input.is_action_just_pressed(string_shift_up):
 			vehicle_node.manual_shift(1)
-
+	
 	if string_shift_down != "":
 		if Input.is_action_just_pressed(string_shift_down):
 			vehicle_node.manual_shift(-1)
-
-	if vehicle_node.automatic_transmission:
-		var stopped = abs(vehicle_node.speed) < STOP_SPEED
-		var brake_down = Input.get_action_strength(string_brake_input) > INPUT_THRESHOLD
-		var throttle_down = Input.get_action_strength(string_throttle_input) > INPUT_THRESHOLD
-
-		if stopped and brake_down and not _brake_was_down and vehicle_node.current_gear > 0:
-			vehicle_node.current_gear = -1
-		elif stopped and throttle_down and not _throttle_was_down and vehicle_node.current_gear < 0:
-			vehicle_node.current_gear = 1
-
-		_brake_was_down = brake_down
-		_throttle_was_down = throttle_down
+	
+	# Reverse gear logic
 
 	if vehicle_node.current_gear == -1:
-		var actual_throttle = vehicle_node.throttle_input
-		vehicle_node.throttle_input = vehicle_node.brake_input
-		vehicle_node.brake_input = actual_throttle
+		vehicle_node.brake_input = Input.get_action_strength(string_throttle_input)
+		vehicle_node.throttle_input = Input.get_action_strength(string_brake_input)
